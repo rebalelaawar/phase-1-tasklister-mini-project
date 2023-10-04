578ebbefc4c2511e28034c69413f735a2451bad4
rebalelaawar <rebalelaawar@gmail.com>
2023-10-04 17:15:29 -0400

    Event Listener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const descriptionTask = document.getElementById("create-task-form")
+  descriptionTask.addEventListener("submit",(e)=> {
+    e.preventDefault();
+
+  const descriptionBar = document.getElementById("new-task-description")
+
+  const descriptionBarInput = descriptionBar.value
+
+  const li = document.createElement("li")
+  li.textContent = descriptionBarInput
+
+  const taskList = document.getElementById("tasks")
+  taskList.appendChild(li)
+  console.log(taskList)
+
+  
+  const buttonElement = document.createElement('button');
+  buttonElement.textContent = " X "
+  li.appendChild(buttonElement)
+
+  buttonElement.addEventListener("click", () => {
+   taskList.removeChild (li)
+  })
+
+
+  
+
+  
+
+  
+
+
+})
 });
+
+
+ 
+
